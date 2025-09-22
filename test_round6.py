7cf1e80d1655db3cf8dc8c9a67e6e8200459cdd7
jasonbdt <ja.bladt@gmail.com>
2025-09-22 23:39:23 +0200

    Add unit tests for round6

--- test_round6.py
+++ test_round6.py
@@ -1,2 +1,13 @@
 import pytest
 from main import round6
+
+
+def test_round6_rounds_up():
+    assert round6(9.7) == 10
+
+
+def test_round6_rounds_down():
+    assert round6(8.5) == 8
+
+
+pytest.main()
