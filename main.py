19a586f93d5caf73e63cba972173c39983e28aa6
jasonbdt <ja.bladt@gmail.com>
2025-09-22 23:33:51 +0200

    Add function to test

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 
+def round6(num):
+    """This function has a bug in it"""
+    return int(num + .6)
+
+
 def main() -> None:
-    pass
+    print(round6(3.5)) # 3
+    print(round6(3.6)) # 4
 
 
 if __name__ == '__main__':
